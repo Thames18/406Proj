42d5bfa8ae73a69f32274f55b1777166a61ea6ba
jonphu <37915090+jonphu@users.noreply.github.com>
2018-04-02 18:23:32 -0400

    Add files via upload

--- Course.java
+++ Course.java
@@ -4,17 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Course {
-	String[] answers;
-	public Course(){
+	String courseCode;
+	String profName;
+	String courseDescription;
+	//linked list of question objects HERE
+	
+	public Course(String code, String name, String description){
 		
-	}
-	public void createQuestionAnswers(String aCourseCode, String[] ar, int correct){
-		courses.put(aCourseCode, new Question(   ));
 	}
 	
-	public void createQuestionAnswers(){
+	//POSSIBLE HELPER FUNCTIONS YOU MIGHT NEED
+	
+	//public void createQuestionAnswers(String aCourseCode, String[] ar, int correct){
+	//	courses.put(aCourseCode, new Question(   ));
+	//}
+	
+	//public void createQuestionAnswers(){
 		
-	}
+	//}
 	
 	
 }
